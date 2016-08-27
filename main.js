6298cad11bd23d80f27c07b954fa98b6f47e3580
alewin <ko.alessio@gmail.com>
2016-08-27 18:23:07 +0200

    debug mode enabled until getmp3 is on beta

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
- const { app, BrowserWindow } = require('electron');
+ const {app,BrowserWindow} = require('electron');
+
  let win;
+
  function createWindow() {
-     win = new BrowserWindow({ width: 800, height: 600 });
+     win = new BrowserWindow({
+         width: 800,
+         height: 600
+
+     });
+     win.openDevTools();
      win.loadURL(`file://${__dirname}/index.html`);
  }
  app.on('ready', createWindow);
